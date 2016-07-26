76bd514cd4ddf1687438c0d2d6b274427393422b
wLabyr <sBum.Seo@gmail.com>
2016-07-27 04:28:29 +0900

    Play 버튼 클릭 시 FirstFloor 씬전환

---
+++ Assets/Scripts/Main.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void ClickPlayButton() {
+		SceneManager.LoadScene ("Scenes/FirstFloor");
+	}
+}
