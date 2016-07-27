02309a3d4d4be3258ddd67030ca3a61f3ef35b70
wLabyr <sBum.Seo@gmail.com>
2016-07-27 14:48:57 +0900

    Intro 추가

--- Assets/Scripts/Main.cs
+++ Assets/Scripts/Main.cs
@@ -4,16 +4,6 @@ using System.Collections;
 
 public class Main : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
-
 	public void ClickPlayButton() {
 		SceneManager.LoadScene ("Scenes/FirstFloor");
 	}
---
+++ Assets/Scripts/ScreenFadeInOut.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class ScreenFadeInOut : MonoBehaviour {
+	public float fadeSpeed = -1f;
+	SpriteRenderer[] spriteFadeList;
+
+	void Start() {
+		spriteFadeList = GetComponentsInChildren<SpriteRenderer>();
+	}
+
+	void Update() {
+		foreach(SpriteRenderer sprite in spriteFadeList) {
+            float alpha = sprite.color.a + fadeSpeed * Time.deltaTime;
+
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, alpha);
+            if(alpha < -0.5f)
+                SceneManager.LoadScene("Scenes/Main");
+        }
+	}
+
+    void FadeStart() {
+        GameObject goEmpty = new GameObject("FadeObject");
+        goEmpty.AddComponent<ScreenFadeInOut>();
+
+        SpriteRenderer[] spriteList = GetComponentsInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer sprite in spriteList) {
+            sprite.tag = "";
+            Collider2D[] col2DList = GetComponentsInChildren<Collider2D>();
+            foreach (Collider2D col2D in col2DList) {
+                col2D.enabled = false;
+            }
+        }
+        goEmpty.transform.position = transform.position;
+    }
+}
