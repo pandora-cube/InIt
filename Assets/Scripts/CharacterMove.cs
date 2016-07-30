4987d04693d00e268515b3d90756203331640bb6
wLabyr <sBum.Seo@gmail.com>
2016-07-30 09:13:05 +0900

    화면 이동 시 맵 바깥을 벗어나지 않도록 함

--- Assets/Scripts/CharacterMove.cs
+++ Assets/Scripts/CharacterMove.cs
@@ -4,26 +4,35 @@ using System.Collections;
 public class CharacterMove : MonoBehaviour {
     public float moveSpeed = 3f;    // 이동 속도
     public int animSpeed = 3;       // 애니메이션 재생 속도
-    public new GameObject camera;   // 카메라 오브젝트
     public Sprite[] runningSprites; // 달리기 스프라이트
-
-    float cameraZ;                  // 카메라의 Z 좌표
+    
+    float backgroundWidth;          // 배경의 두께
+    float backgroundHeight;         // 배경의 높이
+    float cameraWidth;              // 화면의 두께
+    float cameraHeight;             // 화면의 높이
     Sprite standingSprite;          // 정지상태 스프라이트
     bool canmove = true;            // 캐릭터 이동 가능 여부
     int animIndex;                  // 애니메이션 재생 인덱스
     
 	void Start () {
         // 초기화
-        cameraZ = camera.transform.position.z;
+        backgroundWidth = GameObject.Find("Background").GetComponent<SpriteRenderer>().bounds.size.x;
+        backgroundHeight = GameObject.Find("Background").GetComponent<SpriteRenderer>().bounds.size.y;
+        cameraHeight = 2f * Camera.main.orthographicSize;
+        cameraWidth = cameraHeight * Camera.main.aspect;
         standingSprite = GetComponent<SpriteRenderer>().sprite;
 	    animIndex = 0;
+        
+        // 디버깅 메시지 출력
+        Debug.Log(string.Format("Background: {0}, {1}", backgroundWidth, backgroundHeight));
+        Debug.Log(string.Format("Camera: {0}, {1}", cameraWidth, cameraHeight));
 	}
 	
 	void Update () {
         /*
          *  CharacterMove.Update()
          *      캐릭터 이동
-         *      카메라 이동
+         *      화면 이동
          */
         
         // 이동 불가능한 경우
@@ -78,10 +87,23 @@ public class CharacterMove : MonoBehaviour {
             charspr.sprite = runningSprites[animIndex/animSpeed];
         }
         
-        /* 카메라 이동 */
-        Vector3 charpos = charspr.transform.position;
-        // 캐릭터 좌표와 동기화
-        camera.transform.position = new Vector3(charpos.x, charpos.y, cameraZ);
+        /* 화면 이동 */
+        Vector3 charpos = charspr.transform.position;       // 캐릭터의 좌표
+        Vector3 campos = Camera.main.transform.position;    // 화면의 좌표
+        Vector3 campos_moved = campos;                      // 화면의 이동될 좌표
+        float left = charpos.x - cameraWidth/2f;            // 화면의 왼쪽 끝
+        float right = charpos.x + cameraWidth/2f;           // 화면의 오른쪽 끝
+        float bottom = charpos.y - cameraHeight/2f;         // 화면의 아랫쪽 끝
+        float top = charpos.y + cameraHeight/2f;            // 화면의 윗쪽 끝
+
+        // 화면의 좌우 검사
+        if(left >= -backgroundWidth/2f && right <= backgroundWidth/2f)
+            campos_moved.x = charpos.x;
+        // 화면의 상하 검사
+        if(bottom >= -backgroundHeight/2f && top <= backgroundHeight/2f)
+            campos_moved.y = charpos.y;
+        // 화면 좌표와 캐릭터 좌표와 동기화
+        Camera.main.transform.position = new Vector3(campos_moved.x, campos_moved.y, campos_moved.z);
 	}
 
     void onCollisionEnter2D(Collision2D col) {
