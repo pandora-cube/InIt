220c3e56f8dde200a37ba80303a463f464be46c6
wLabyr <sBum.Seo@gmail.com>
2016-08-05 01:30:42 +0900

    화면 크기 조정 (960 x 540)

--- Assets/Scripts/Scenes/Intro.cs
+++ Assets/Scripts/Scenes/Intro.cs
@@ -3,6 +3,11 @@ using UnityEngine.SceneManagement;
 using System.Collections;
 
 public class Intro : MonoBehaviour {
+    void Start() {
+        // 화면 크기 조정 (960 x 540)
+        Screen.SetResolution(960, 540, false);
+    }
+
     public void FadeEnded() {
         SceneManager.LoadScene("Scenes/Main");
     }
