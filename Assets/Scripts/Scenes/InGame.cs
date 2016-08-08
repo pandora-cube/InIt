e352b58251d8d89d1054126da067fac4e49e7f30
wLabyr <sBum.Seo@gmail.com>
2016-08-08 22:37:32 +0900

    메뉴 Fade In, Fade Out, Move 애니메이션 적용

--- Assets/Scripts/Scenes/InGame.cs
+++ Assets/Scripts/Scenes/InGame.cs
@@ -2,6 +2,8 @@
 using System.Collections;
 
 public class InGame : MonoBehaviour {
+    bool menuZoomIn = true;
+
     void Start() {
         GameObject.Find("Screen Fader").transform.localScale = new Vector3(0f, 0f, 0f);
         GameObject.Find("Menu UI").transform.localScale = new Vector3(0f, 0f, 0f);
@@ -9,18 +11,21 @@ public class InGame : MonoBehaviour {
     
 	void Update() {
         Transform fader = GameObject.Find("Screen Fader").transform;
+        Transform menu = GameObject.Find("Menu UI").transform;
         
 	    if(Input.GetKeyDown(KeyCode.Escape)) {
             // Screen Fader가 비활성화 되어 있는 경우
             if(fader.localScale == new Vector3(0f, 0f, 0f)) {
-                // Screen Fader를 투명하게 함
-                // (ScreenFadeInOut 컴포넌트에 의해 Fade In)
+                // Screen Fader Fade In
                 fader.GetComponent<SpriteRenderer>().color = new Color(0f, 0f, 0f, 0f);
+                fader.GetComponent<ScreenFadeInOut>().endOpacity = .6f;
                 // Screen Fader 활성화
                 fader.localScale = new Vector3(2048f, 2048f, 1f);
                 
-                // Menu UI 활성화
-                GameObject.Find("Menu UI").transform.localScale = new Vector3(1f, 1f, 1f);
+                // Menu UI Zoom In
+                menuZoomIn = true;
+                menu.localScale = new Vector3(1f, 1f, 1f);
+                menu.position = new Vector3(menu.position.x, 500f, menu.position.z);
 
                 // 캐릭터 이동 불가
                 GameObject.Find("Character").GetComponent<CharacterMove>().canmove = false;
@@ -28,13 +33,40 @@ public class InGame : MonoBehaviour {
                 Resume();
             }
         }
+        
+        if(menu.position.y > 0f) {
+            float moved_y = menu.position.y - Time.deltaTime * 750;
+
+            if(moved_y <= 0f)
+                menu.position = new Vector3(menu.position.x, 0f, menu.position.z);
+            else
+                menu.position = new Vector3(menu.position.x, moved_y, menu.position.z);
+        }
+
+        /*if(menu.localScale.x > 0f && menu.localScale.x != 1f) {
+            if(menuZoomIn) {
+                if(menu.localScale.x+Time.deltaTime >= 1f)
+                    menu.localScale = new Vector3(1f, 1f, 1f);
+                else
+                    menu.localScale = new Vector3(menu.localScale.x+2*Time.deltaTime, menu.localScale.y+2*Time.deltaTime, 1f);
+            }
+        }*/
 	}
 
     public void Resume() {
-        // Screen Fader, Menu UI 비활성화
-        GameObject.Find("Screen Fader").transform.localScale = GameObject.Find("Menu UI").transform.localScale = new Vector3(0f, 0f, 0f);
+        // Screen Fader Fade Out
+        GameObject.Find("Screen Fader").GetComponent<ScreenFadeInOut>().endOpacity = 0f;
+        // Menu UI 비활성화
+        GameObject.Find("Menu UI").transform.localScale = new Vector3(0f, 0f, 0f);
 
         // 캐릭터 이동 가능
         GameObject.Find("Character").GetComponent<CharacterMove>().canmove = true;
+    }
+
+    void FadeEnded() {
+        float opacity = GameObject.Find("Screen Fader").GetComponent<SpriteRenderer>().color.a;
+        
+        if(opacity == 0f)
+            GameObject.Find("Screen Fader").transform.localScale = new Vector3(0f, 0f, 0f);
     }
 }
--- Assets/Scripts/ScreenFadeInOut.cs
+++ Assets/Scripts/ScreenFadeInOut.cs
@@ -9,14 +9,17 @@ public class ScreenFadeInOut : MonoBehaviour {
 	void Update() {
         // 페이드가 적용될 스프라이트
         SpriteRenderer sprite = GetComponent<SpriteRenderer>();
+        // 페이드 속도의 절대값
+        float deltaSpeed = Mathf.Abs(fadeSpeed) * Time.deltaTime;
         // 페이드 진행 시간에 따른 투명도 값 계산
-        float opacity = sprite.color.a + fadeSpeed * Time.deltaTime;
-
+        float opacity = sprite.color.a + (sprite.color.a < endOpacity ? deltaSpeed : -deltaSpeed);
+        
         // 페이드 종료 콜백 호출
-        if((fadeSpeed < 0f && opacity < endOpacity) || (fadeSpeed >= 0f && opacity > endOpacity))
+        if(sprite.color.a >= endOpacity-.05f && sprite.color.a <= endOpacity+.05f) {
+            sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, endOpacity);
             foreach(Transform obj in GameObject.FindObjectsOfType<Transform>())
                 obj.SendMessage("FadeEnded", SendMessageOptions.DontRequireReceiver);
-        else {
+        } else {
             // 스프라이트에 투명도 적용
             sprite.color = new Color(sprite.color.r, sprite.color.g, sprite.color.b, opacity);
         }
