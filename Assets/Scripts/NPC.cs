49f33f5b3aff1405c8899315103895f8390c6cd1
wLabyr <sBum.Seo@gmail.com>
2016-08-13 01:43:42 +0900

    NPC.cs, Dialogue.cs 버그 수정

--- Assets/Scripts/NPC.cs
+++ Assets/Scripts/NPC.cs
@@ -8,22 +8,17 @@ public class NPC : MonoBehaviour {
     public string[] Messages;       // NPC의 대사 배열
     public Sprite Poster;           // 포스터 스프라이트
     public bool dontChat = false;   // 대화 거부 등의 대사 수행 여부
-    
-    int messageIndex = 0;           // 대사 인덱스
+
+    void Start() {
+        // 대화 가능 영역 검사 용도의 Collider 생성
+        /*BoxCollider2D trigger = Component.Instantiate<BoxCollider2D>(GetComponent<BoxCollider2D>());
+        // Trigger로 설정
+        trigger.isTrigger = true;
+        // 영역을 충돌 처리 Collider보다 (1f, 1f) 크게 설정
+        trigger.size.Set(trigger.size.x+1f, trigger.size.y+1f);*/
+    }
 
     void Update() {
-        /*
-         *  NPC.Update()
-         *      대화 처리
-         *      레벨에 따른 네임태그 강조
-         */
-        
-        /* 대화 처리 */
-        // Space 혹은 Enter 키를 누른 경우
-        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
-            NPCTalk();
-        }
-
         /* 레벨에 따른 네임태그 강조 */
         try {
             // 다음 진행 상대가 이 NPC인 경우
@@ -35,66 +30,5 @@ public class NPC : MonoBehaviour {
                 GetComponent<Nametag>().Blink = false;
             }
         } catch { }
-    }
-
-    public void NPCTalk() {
-        // 대화 UI
-        Dialogue dialogue = GameObject.Find("Dialogue UI").GetComponent<Dialogue>();
-        // 포스터 이미지
-        Transform poster = dialogue.transform.FindChild("Canvas").FindChild("Poster");
-        
-        // 메뉴가 활성화되어 있는 경우
-        if(GameObject.Find("Menu UI").transform.localScale == new Vector3(1f, 1f, 1f)) {
-        }
-        // 대사가 출력중인 경우
-        else if(dialogue.msgCount != 0) {
-        }
-        // NPC가 대화를 거부하였거나 더 할 대사가 없는 경우
-        else if(messageIndex == -1 || messageIndex >= Messages.Length) {
-            // 대화 UI 숨김
-            dialogue.HideDialogue();
-            // 레벨 설정
-            if(messageIndex != -1)
-                SingleTone.Instance.Level = Stage;
-            // 대사 인덱스 초기화
-            messageIndex = 0;
-        }
-        // NPC와 접촉해 있는 경우
-        else if(Messages.Length > 0) {
-            // 이 NPC와 대화할 적정 단계인 경우
-            if(SingleTone.Instance.Level == Stage-1) {
-                string message = Messages[messageIndex++];
-
-                // 포스터 이미지 출력 명령인 경우
-                if(message == "Poster") {
-                    // 포스터 이미지 출력
-                    poster.FindChild("Image").GetComponent<Image>().sprite = Poster;
-                    poster.localScale = new Vector3(1f, 1f, 1f);
-                } else {
-                    // 대화 UI 출력
-                    dialogue.ShowDialogue(Name, message);
-                }
-            }
-            // 플레이어의 진행도가 낮은 경우
-            else if(SingleTone.Instance.Level < Stage-1) {
-                // 대화 거부 처리
-                messageIndex = -1;
-                // 대화 UI 출력
-                if(dontChat)
-                    NPCTalk();
-                else
-                    dialogue.ShowDialogue(Name, "나 말고 다른 사람한테 먼저 가봐.");
-            }
-            // 플레이어의 진행도가 높은 경우
-            else if(SingleTone.Instance.Level > Stage-1) {
-                // 대화 거부 처리
-                messageIndex = -1;
-                // 대화 UI 출력
-                if(dontChat)
-                    NPCTalk();
-                else
-                    dialogue.ShowDialogue(Name, "난 이제 해 줄 말이 없어.");
-            }
-        }
     }
 }
--- Assets/Scripts/UI/Dialogue.cs
+++ Assets/Scripts/UI/Dialogue.cs
@@ -6,14 +6,16 @@ public class Dialogue : MonoBehaviour {
     public float printSpeed = 0.1f;     // 메시지 출력 속도
     public float blinkSpeed = 2f;       // Blinker의 페이드 속도
     public float blinkOpacity = 1f;     // Blinker의 최대 투명도
-    public string[] Messages;           // 출력될 메시지
     public int msgCount = 0;            // 메시지 출력 카운트
 
     Transform uiObj;                    // UI 오브젝트
     Transform dialogueObj;              // 메시지 오브젝트
     Transform posterObj;                // 포스터 오브젝트
+    string contactedNPC = string.Empty; // 현재 대화중인 NPC
     string msgResult;                   // 메시지 내용
     int msgLength;                      // 메시지 길이
+    int msgIndex = 0;                   // 메시지 인덱스
+    bool talkToggle = false;            // 키 인식의 빠른 연속 방지
     float blinkSpeedTmp;                // Blinker의 페이드 속도
     float blinkOpacityTmp;              // Blinker의 최대 투명도
 
@@ -31,6 +33,13 @@ public class Dialogue : MonoBehaviour {
     }
 
     void Update() {
+        /* NPC 대화 처리 */
+        // Space 혹은 Enter 키를 누른 경우
+        if(Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Return)) {
+            Talk();
+        }
+
+        /* Blinker 깜빡거리기 */
         // Dialogue UI 우측 하단의 깜빡이
         Text blinker = dialogueObj.FindChild("Blinker").GetComponent<Text>();
         // 시간에 따른 투명도
@@ -47,7 +56,97 @@ public class Dialogue : MonoBehaviour {
     }
 
     public void OnClick() {
-        GameObject.Find(SingleTone.Instance.collidedNPC).GetComponent<NPC>().NPCTalk();
+        Talk();
+    }
+
+    void ToggleTalk() {
+        talkToggle = false;
+    }
+
+    public void Talk() {
+        if(talkToggle)
+            return;
+        talkToggle = true;
+        Invoke("ToggleTalk", .5f);
+
+        // 포스터 이미지
+        Transform poster = transform.FindChild("Canvas").FindChild("Poster");
+        // 상대 NPC 이름
+        string npcName = SingleTone.Instance.collidedNPC.Length > 0 ? SingleTone.Instance.collidedNPC : contactedNPC;
+        // 상대 NPC
+        NPC npc;
+        
+        // 충돌 유지중이거나 대화중인 NPC가 없는 경우
+        if(npcName.Length < 1) {
+            if(transform.localScale == new Vector3(1f, 1f, 1f))
+                HideDialogue();
+            Debug.Log("[Talk Debug] 상대 NPC 없음");
+            return;
+        } else
+            npc = GameObject.Find(npcName).GetComponent<NPC>();
+
+        // 메뉴가 활성화되어 있는 경우
+        if(GameObject.Find("Menu UI").transform.localScale == new Vector3(1f, 1f, 1f)) {
+            Debug.Log("[Talk Debug] 메뉴 활성화되어 있음");
+        }
+        // 대사가 출력중인 경우
+        else if(msgCount != 0) {
+            Debug.Log("[Talk Debug] 대사 출력중");
+        }
+        // NPC가 대화를 거부하였거나 더 할 대사가 없는 경우
+        else if(msgIndex == -1 || msgIndex >= npc.Messages.Length) {
+            Debug.Log("[Talk Debug] 대화 거부 OR 대사 없음");
+
+            // 상대 NPC 없음
+            contactedNPC = string.Empty;
+            // 대화 UI 숨김
+            HideDialogue();
+            // 레벨 설정
+            if(msgIndex != -1)
+                SingleTone.Instance.Level = npc.Stage;
+            // 대사 인덱스 초기화
+            msgIndex = 0;
+        }
+        // 대사가 있는 경우
+        else if(npc.Messages.Length > 0) {
+            // 상대 NPC 이름
+            contactedNPC = npc.transform.name;
+
+            // 이 NPC와 대화할 적정 단계인 경우
+            if(SingleTone.Instance.Level == npc.Stage-1) {
+                string message = npc.Messages[msgIndex++];
+
+                // 포스터 이미지 출력 명령인 경우
+                if(message == "Poster") {
+                    // 포스터 이미지 출력
+                    poster.FindChild("Image").GetComponent<Image>().sprite = npc.Poster;
+                    poster.localScale = new Vector3(1f, 1f, 1f);
+                } else {
+                    // 대화 UI 출력
+                    ShowDialogue(npc.Name, message);
+                }
+            }
+            // 플레이어의 진행도가 낮은 경우
+            else if(SingleTone.Instance.Level < npc.Stage-1) {
+                // 대화 거부 처리
+                msgIndex = -1;
+                // 대화 UI 출력
+                if(npc.dontChat)
+                    Talk();
+                else
+                    ShowDialogue(npc.Name, "나 말고 다른 사람한테 먼저 가봐.");
+            }
+            // 플레이어의 진행도가 높은 경우
+            else if(SingleTone.Instance.Level > npc.Stage-1) {
+                // 대화 거부 처리
+                msgIndex = -1;
+                // 대화 UI 출력
+                if(npc.dontChat)
+                    Talk();
+                else
+                    ShowDialogue(npc.Name, "난 이제 해 줄 말이 없어.");
+            }
+        }
     }
 
     void PrintMessage() {
@@ -67,7 +166,7 @@ public class Dialogue : MonoBehaviour {
         }
     }
 
-    public void HideDialogue() {
+    void HideDialogue() {
         // 오브젝트 숨김
         uiObj.localScale = dialogueObj.localScale = posterObj.localScale = new Vector3(0f, 0f, 0f);
         // 타이머 강제 해제
@@ -78,7 +177,7 @@ public class Dialogue : MonoBehaviour {
         msgResult = string.Empty;
     }
 
-    public void ShowDialogue(string name, string result) {
+    void ShowDialogue(string name, string result) {
         // 이름 텍스트 설정
         dialogueObj.FindChild("Name").GetComponent<Text>().text = name;
         // Dialogue UI 활성화
