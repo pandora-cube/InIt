ea021f42876e4e3dcbed03a8521699ee6c465405
wLabyr <sBum.Seo@gmail.com>
2016-08-15 23:00:02 +0900

    데이터 없을 때 불러오기를 하면 시작 시각이 기록되지 않는 버그 수정

--- Assets/Scripts/UI/GameStart.cs
+++ Assets/Scripts/UI/GameStart.cs
@@ -142,6 +142,7 @@ public class GameStart : MonoBehaviour {
                 GameObject.Find("Delete Button").transform.localScale = new Vector3(0f, 0f, 0f);
             }
             else {
+                Debug.Log(temp.PlayTime.ToString());
                 int ptime = (int)temp.PlayTime;
                 string ptimetext = string.Empty;
             
@@ -171,7 +172,7 @@ public class GameStart : MonoBehaviour {
     }
 
     public void Play() {
-        PlayerData.flagLoadPlayerData = gsSlider.value == 1;
+        PlayerData.flagLoadPlayerData = gsSlider.value == 1 && PlayerData.ReadPlayerData() != null;
         SceneManager.LoadScene("Scenes/FirstFloor");
     }
 }
