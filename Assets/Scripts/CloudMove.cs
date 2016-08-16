ddd357bb25b4cdbb4d3a80e5afd99ce0e28a5a18
wLabyr <sBum.Seo@gmail.com>
2016-08-16 17:14:38 +0900

    옥상 맵 추가
    - 구름은 0.01f 속도, 왼쪽에서 오른쪽으로 흘러감

---
+++ Assets/Scripts/CloudMove.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CloudMove : MonoBehaviour {
+    public float scrollSpeed = 0.2f;
+    float Offset;
+
+	void Update() {
+        Offset += Time.deltaTime * scrollSpeed;
+        GetComponent<Renderer>().material.mainTextureOffset = new Vector2(-Offset, 0);
+	}
+}
