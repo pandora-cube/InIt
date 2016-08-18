a69e76a6943915cdd6f02ec3c7dd0cb9305c7d13
wLabyr <sBum.Seo@gmail.com>
2016-08-18 23:19:41 +0900

    네임태그 좌표 자동 동기화

--- Assets/Scripts/UI/Nametag.cs
+++ Assets/Scripts/UI/Nametag.cs
@@ -7,19 +7,14 @@ public class Nametag : MonoBehaviour {
     public string tagText;          // 네임태그의 텍스트
     public bool Blink = false;      // 이 속성이 참이면 네임태그가 강조 처리됨
 
+    Object tag;                     // 네임태그 오브젝트
     string tagName;                 // 네임태그 오브젝트의 이름
     Color originalColor;            // 네임태그의 초기 색상
     bool blinkMode = true;          // 이 속성이 참이면 강조 처리중 네임태그 색상의 R값이 더해짐
 
 	void Start() {
-        // 부모 오브젝트의 좌표
-        Vector3 parentPosition = transform.position;
-        // 네임태그의 좌표
-        // 네임태그가 화면상 가장 앞에 오도록 Z좌표를 설정한다.
-        Vector3 tagPosition = new Vector3(parentPosition.x, parentPosition.y+tagOffset, -3f);
-
         // 네임태그 오브젝트 생성
-	    Object tag = Instantiate(Prefab, tagPosition, transform.rotation);
+	    tag = Instantiate(Prefab, new Vector3(0f, 0f), transform.rotation);
         // 네임태그 오브젝트의 이름 설정
         tag.name = tagName =  "Nametag/" + transform.tag + "/" + transform.name;
 
@@ -33,8 +28,22 @@ public class Nametag : MonoBehaviour {
 	}
 	
 	void Update() {
+        /*
+         *  Nametag.Update()
+         *      좌표 동기화
+         *      Blink
+         */
+        
+        /* 좌표 동기화 */
+        // 부모 오브젝트의 좌표
+        Vector3 parentPosition = transform.position;
+        // 네임태그의 좌표
+        // 네임태그가 화면상 가장 앞에 오도록 Z좌표를 설정한다.
+        GameObject.Find(tag.name).transform.position = new Vector3(parentPosition.x, parentPosition.y+tagOffset, -3f);
+
         TextMesh text = GameObject.Find(tagName).GetComponent<TextMesh>();
 
+        /* Blink */
 	    if(Blink) {
             float b = text.color.b + (blinkMode ? Time.deltaTime : -Time.deltaTime) / 2f;
             
