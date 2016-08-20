fe3d705495c2e909a7469a798ccb7af3e0001d5f
wLabyr <sBum.Seo@gmail.com>
2016-08-21 00:58:04 +0900

    3층-옥상 출입구
    미스터리 영예과 끝나면 항상 열림

--- Assets/Scripts/Events/Film_Artist.cs
+++ Assets/Scripts/Events/Film_Artist.cs
@@ -7,6 +7,11 @@ public class Film_Artist : MonoBehaviour {
 	void Start () {
         originalPos = transform.position;
 
+        if(PlayerData.Player.Level >= GetComponent<NPC>().Stage-1) {
+            GameObject.Find("3F-Rooftop/Outside").GetComponent<Entrance>().Locked = false;
+            GameObject.Find("3F-Rooftop/Inside").GetComponent<Entrance>().Locked = false;
+        }
+
 	    GetComponent<NPC>().dontChat = true;
 	}
 	
@@ -16,7 +21,11 @@ public class Film_Artist : MonoBehaviour {
             PlayerData.Player.Level = GameObject.Find("Mingyeong_Kim").GetComponent<NPC>().Stage-1;
             GameObject.Find("Dialogue UI").GetComponent<Dialogue>().ShowDialogue("실패", "미스터리 영예과가 옥상 문을 잠궜습니다.");
         } else if(PlayerData.Player.Level == GetComponent<NPC>().Stage-2) {
+            GameObject.Find("3F-Rooftop/Outside").GetComponent<Entrance>().Locked = false;
+            GameObject.Find("3F-Rooftop/Inside").GetComponent<Entrance>().Locked = false;
+
             PlayerData.Player.Level = GetComponent<NPC>().Stage-1;
+
             GetComponent<NPC>().dontChat = false;
         }
     }
