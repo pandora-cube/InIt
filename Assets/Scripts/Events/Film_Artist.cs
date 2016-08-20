a8c6a3d2c030c511351b8e08181cfd499585a05a
wLabyr <sBum.Seo@gmail.com>
2016-08-21 00:17:36 +0900

    미스터리 영예과
    미션 실패 시 원래 위치로 텔레포트

--- Assets/Scripts/Events/Film_Artist.cs
+++ Assets/Scripts/Events/Film_Artist.cs
@@ -2,13 +2,17 @@
 using System.Collections;
 
 public class Film_Artist : MonoBehaviour {
+    Vector3 originalPos;
+
 	void Start () {
+        originalPos = transform.position;
+
 	    GetComponent<NPC>().dontChat = true;
 	}
 	
 	void OnNPCCommandsEnd() {
         if(PlayerData.Player.Location != "Background_세종관 옥상") {
-            Debug.Log(PlayerData.Player.Location);
+            transform.position = originalPos;
             PlayerData.Player.Level = GameObject.Find("Mingyeong_Kim").GetComponent<NPC>().Stage-1;
             GameObject.Find("Dialogue UI").GetComponent<Dialogue>().ShowDialogue("실패", "미스터리 영예과가 옥상 문을 잠궜습니다.");
         } else if(PlayerData.Player.Level == GetComponent<NPC>().Stage-2) {
