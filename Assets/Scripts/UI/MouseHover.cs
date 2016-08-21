8858d5ab3a88100a13fe6dfbf944f0f52e5a057c
wLabyr <sBum.Seo@gmail.com>
2016-08-21 12:59:30 +0900

    마우스 올릴 시 버튼 크기 증가

---
+++ Assets/Scripts/UI/MouseHover.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using System.Collections;
+
+public class MouseHover : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler {
+    public float maxScale = 1.05f;
+    bool Hovered = false;
+
+    void Update() {
+        float scale = transform.localScale.x;
+        float delta = Time.deltaTime;
+
+        if(Hovered)
+            scale = scale+delta < maxScale ? scale+delta : maxScale;
+        else
+            scale = scale-delta > 1f ? scale-delta : 1f;
+        transform.localScale = new Vector3(scale, scale, 1f);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData) {
+        Hovered = true;
+    }
+
+    public void OnPointerExit(PointerEventData eventData) {
+        Hovered = false;
+    }
+}
