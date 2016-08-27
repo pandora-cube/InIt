b0c356955f4d6b8edc6c7e8371507d7a7bdf720a
wLabyr <sBum.Seo@gmail.com>
2016-08-28 00:15:32 +0900

    Layer.cs 추가

---
+++ Assets/Scripts/Layer.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Layer : MonoBehaviour {
+	void Update() {
+        Vector3 charPosition = GameObject.Find("Character").transform.position;
+        Vector3 myPosition = transform.localPosition;
+        
+        transform.localPosition = new Vector3(myPosition.x, myPosition.y, myPosition.y > charPosition.y ? -1 : -2);
+	}
+}
