b65c9cb9783abae97b3c1848a9f421beb633d98a
wLabyr <sBum.Seo@gmail.com>
2016-08-28 12:57:56 +0900

    강의실 레이어화

--- Assets/Scripts/Layer.cs
+++ Assets/Scripts/Layer.cs
@@ -3,9 +3,12 @@ using System.Collections;
 
 public class Layer : MonoBehaviour {
 	void Update() {
-        Vector3 charPosition = GameObject.Find("Character").transform.position;
-        Vector3 myPosition = transform.localPosition;
+        Transform character = GameObject.Find("Character").transform;
+        Vector3 charPosition = character.position;
+        Vector3 myPosition = transform.position;
+        float charFoot = charPosition.y - character.GetComponent<SpriteRenderer>().bounds.size.y * character.localScale.y / 2f - .25f; // .25f는 캐릭터 이미지의 여백을 고려한 것
+        float myFoot = myPosition.y - GetComponent<SpriteRenderer>().bounds.size.y * transform.localScale.y / 2f;
         
-        transform.localPosition = new Vector3(myPosition.x, myPosition.y, myPosition.y > charPosition.y ? -1 : -2);
+        transform.position = new Vector3(myPosition.x, myPosition.y, myFoot > charFoot ? -1 : -3);
 	}
 }
