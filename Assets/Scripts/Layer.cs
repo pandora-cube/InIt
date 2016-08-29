961d07d9241545dcf55c8c48bbe9bd1aee8275e7
wLabyr <sBum.Seo@gmail.com>
2016-08-29 23:35:38 +0900

    NPC 스프라이트 상하좌우 여백 제거

--- Assets/Scripts/Layer.cs
+++ Assets/Scripts/Layer.cs
@@ -6,7 +6,7 @@ public class Layer : MonoBehaviour {
         Transform character = GameObject.Find("Character").transform;
         Vector3 charPosition = character.position;
         Vector3 myPosition = transform.position;
-        float charFoot = charPosition.y - character.GetComponent<SpriteRenderer>().bounds.size.y * character.localScale.y / 2f - .25f; // .25f는 캐릭터 이미지의 여백을 고려한 것
+        float charFoot = charPosition.y - character.GetComponent<SpriteRenderer>().bounds.size.y * character.localScale.y / 2f;
         float myFoot = myPosition.y - GetComponent<SpriteRenderer>().bounds.size.y * transform.localScale.y / 2f;
         
         transform.position = new Vector3(myPosition.x, myPosition.y, myFoot > charFoot ? -1 : -3);
