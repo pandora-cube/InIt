837740fa900b9507bbfd2d5c45b22de6c1ed3c97
wLabyr <sBum.Seo@gmail.com>
2016-08-30 00:23:09 +0900

    레이어 버그 수정

--- Assets/Scripts/Layer.cs
+++ Assets/Scripts/Layer.cs
@@ -6,8 +6,8 @@ public class Layer : MonoBehaviour {
         Transform character = GameObject.Find("Character").transform;
         Vector3 charPosition = character.position;
         Vector3 myPosition = transform.position;
-        float charFoot = charPosition.y - character.GetComponent<SpriteRenderer>().bounds.size.y * character.localScale.y / 2f;
-        float myFoot = myPosition.y - GetComponent<SpriteRenderer>().bounds.size.y * transform.localScale.y / 2f;
+        float charFoot = charPosition.y - character.GetComponent<SpriteRenderer>().bounds.size.y / 2f;
+        float myFoot = myPosition.y - GetComponent<SpriteRenderer>().bounds.size.y / 2f;
         
         transform.position = new Vector3(myPosition.x, myPosition.y, myFoot > charFoot ? -1 : -3);
 	}
